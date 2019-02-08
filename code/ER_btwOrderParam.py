b2c25472f7dd003258fd51495f65cde2d0dd923f
Nahuel Almeira <nahu1990@gmail.com>
2019-02-08 09:59:11 -0300

    Deleted all files and added more ER networks

--- code/ER_btwOrderParam.py
+++ code/ER_btwOrderParam.py
@@ -54,40 +54,59 @@ for seed in range(min_seed, max_seed):
 
             if mode == 0:
                 # s_i^t = b_i^t
-                for t, row in enumerate(spin_matrix):
-                    for i, b_i_t in enumerate(row):
-                        spin_matrix[t][i] = b_i_t
+                for t in range(len(btw_matrix)):
+                    for i in range(len(btw_matrix[0])):
+                        spin_matrix[t][i] = btw_matrix[t][i]
+
+            if False:
+                if mode == 0:
+                    # s_i^t = b_i^t
+                    for t, row in enumerate(spin_matrix):
+                        for i, b_i_t in enumerate(row):
+                            spin_matrix[t][i] = b_i_t
 
             if mode == 1:
                 # s_i^t = b_i^t / b_i^0
-                for t, row in enumerate(spin_matrix):
-                    for i, b_i_t in enumerate(row):
-                        b_i_0 = spin_matrix[0][i]
+                for t in range(len(btw_matrix)):
+                    for i in range(len(btw_matrix[0])):
+                        b_i_t = btw_matrix[t][i]
+                        b_i_0 = btw_matrix[0][i]
                         if b_i_0 != 0:
                             spin_matrix[t][i] = b_i_t / b_i_0
                         else:
                             spin_matrix[t][i] = 0.0
 
+            if False:
+                if mode == 1:
+                    # s_i^t = b_i^t / b_i^0
+                    for t, row in enumerate(spin_matrix):
+                        for i, b_i_t in enumerate(row):
+                            b_i_0 = spin_matrix[0][i]
+                            if b_i_0 != 0:
+                                spin_matrix[t][i] = b_i_t / b_i_0
+                            else:
+                                spin_matrix[t][i] = 0.0
+
             if mode == 2:
                 # s_i^t = b_i^{t+1} / b_i^t
-                for t, row in enumerate(spin_matrix):
-                    if t == len(spin_matrix):
-                        spin_matrix[t] = [0.0 for elem in spin_matrix[t]]
+                for t, row in enumerate(btw_matrix):
+                    if t == len(btw_matrix):
+                        spin_matrix[t] = [0.0 for elem in btw_matrix[t]]
                         break
                     for i, b_t in enumerate(row):
                         if b_t != 0:
-                            spin_matrix[t][i] = spin_matrix[t+1][i] / b_t
+                            spin_matrix[t][i] = btw_matrix[t+1][i] / b_t
 
             if mode == 3:
                 # s_i^t = ln( (b_i^t + 1) / (<b^t> + 1) )
-                for t, row in enumerate(spin_matrix):
+                for t, row in enumerate(btw_matrix):
                     mean_b_t = np.mean(row)
                     for i, b_t in enumerate(row):
                         spin_matrix[t][i] = np.log((b_t + 1) / (mean_b_t + 1))
 
             if mode == 4:
                 # s_i^t = tanh(b_i^t / <b^t>)
-                for t, row in enumerate(spin_matrix):
+                for t, row in enumerate(btw_matrix):
                     mean_b_t = np.mean(row)
                     for i, b_t in enumerate(row):
                         if mean_b_t > 0:
@@ -98,11 +117,11 @@ for seed in range(min_seed, max_seed):
             spin_sum = np.sum(spin_matrix, axis=1)
             spin_sum = np.append(spin_sum, np.repeat(np.NaN, (N-len(spin_sum))))
         
-            spin_var = np.var(spin_matrix, axis=1)
-            spin_var = np.append(spin_var, np.repeat(np.NaN, (N-len(spin_var))))
+            #spin_var = np.var(spin_matrix, axis=1)
+            #spin_var = np.append(spin_var, np.repeat(np.NaN, (N-len(spin_var))))
 
             spin_data.append(spin_sum)
-            spin_data.append(spin_var)
+            #spin_data.append(spin_var)
 
         spin_data = np.array(spin_data)
         output = spin_data.T
